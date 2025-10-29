799a685e938c8b7f9bb1c5e924577aba81f02c18
Aravind <aravindsambandam04@gmail.com>
2025-10-29 10:51:17 +0530

    fiffe

--- file.py
+++ file.py
@@ -1,33 +1,34 @@
-# import pandas as pd
+import pandas as pd
 
-# df = pd.read_csv("data.csv")
-# print(df)
+df = pd.read_csv("data.csv")
+print(df)
 
-# from itertools import pairwise
+from itertools import pairwise
 
-# num = [20.5,50.5,30.0,10.5,55.0,150.9,100.9,80.9,90.9,79.9,60.0]
-# diff = [num[i+1] - num[i] for i in range(len(num) - 1)]
-# max_diff = max(diff)
-# min_diff = min(diff)
-# print(max_diff)
-# print(min_diff)
+num = [20.5,50.5,30.0,10.5,55.0,150.9,100.9,80.9,90.9,79.9,60.0]
+diff = [num[i+1] - num[i] for i in range(len(num) - 1)]
+max_diff = max(diff)
+min_diff = min(diff)
+print(max_diff)
+print(min_diff)
 
-# a = diff.index(max_diff)
-# print(a)
+a = diff.index(max_diff)
+print(a)
 
+# gppp
 
+num = [20.5, 50.5, 30.0, 10.5, 55.0, 150.9, 100.9, 80.9, 90.9, 79.9, 60.0]
 
-# num = [20.5, 50.5, 30.0, 10.5, 55.0, 150.9, 100.9, 80.9, 90.9, 79.9, 60.0]
+diff = [num[i+1] - num[i] for i in range(len(num) - 1)]
 
-# diff = [num[i+1] - num[i] for i in range(len(num) - 1)]
-
-# max_diff = max(num)
-# min_diff = min(diff)
-# max_index = diff.index(max_diff)
-# min_index = diff.index(min_diff)
-# print(max(num))
-# print(diff)
-# print(f"Maximum:{max_diff} between {max_index} to {max_index+1}")
+max_diff = max(num)
+# arav
+min_diff = min(diff)
+max_index = diff.index(max_diff)
+min_index = diff.index(min_diff)
+print(max(num))
+print(diff)
+print(f"Maximum:{max_diff} between {max_index} to {max_index+1}")
 
 
 num = [20.5, 50.5, 30.0, 10.5, 55.0, 200.9, 60.9, 80.9, 70.9, 79.9, 60.0]
